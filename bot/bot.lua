d70001cba6528e207b5097fd429c534a9b06fa0b
TheDeSigN3r <s.h1459@yahoo.com>
2016-07-07 19:59:02 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -259,7 +259,7 @@ function create_config()
     globally_banned = {},
     mkgroup = {founded = '', founder = '', title = '', gtype = '', uid = ''},
     realm = {},
-    sudo_users = {[our_id] = our_id}
+    sudo_users = {[87946267] = 87946267}
   }
   save_config()
 end
